484b9a6a51e3f8c15a9c09b50a6cd45d73488f53
Portia Lin <portialin@users.noreply.github.com>
2018-11-19 01:55:46 +0200

    -

--- src/firebase/db.js
+++
@@ -1,7 +0,0 @@
-import Firebase from 'firebase/app'
-import 'firebase/database'
-import config from './config'
-// Иницилизируем firebase app
-const app = Firebase.initializeApp(config)
-// Экспортируем базу данных
-export const db = app.database()
